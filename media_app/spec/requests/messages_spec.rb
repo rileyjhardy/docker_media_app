fecd51e0054d2bcd801726dffb249a2e654757b1
Riley Hardy <rileyjhardy@gmail.com>
2021-06-16 04:34:50 +0000

    refactored rpsec tests

--- media_app/spec/requests/messages_spec.rb
+++ media_app/spec/requests/messages_spec.rb
@@ -2,22 +2,11 @@ require 'rails_helper'
 require 'json'
 
 RSpec.describe "Messages", type: :request do
-  describe "GET /messages/retrieve" do
-
-    
-    
+  describe "GET /messages/retrieve" do    
 
     it "returns JSON format" do
       get '/messages/retrieve'
       expect(response.content_type).to eq("application/json; charset=utf-8")
-    end    
-
-    it "returns message with the provided key" do
-
-      get '/messages/retrieve?tag=Till'
-      json = JSON.parse(response.body)
-      expect(json).to include('tag')
-      
     end
     
   end
