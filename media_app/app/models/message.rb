88edc3491351466b776d0855fca1eac4d98cc6f3
Riley Hardy <rileyjhardy@gmail.com>
2021-06-15 23:58:10 -0500

    refactored rspec tests

--- media_app/app/models/message.rb
+++ media_app/app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ApplicationRecord
     mount_uploader :thumb_file, ThumbUploader
     validates :title, presence: true
     validates :tag, presence: true
-       
+    validates_format_of :video_file, :with => /'^\.mp4$'/
     scope :filter_by_tag, -> (tag){ where tag: tag }
 end
--- media_app/spec/models/messages_spec.rb
+++ media_app/spec/models/messages_spec.rb
@@ -2,31 +2,24 @@ require 'rails_helper'
 
 RSpec.describe "Messages", type: :model do
 
-    it "checks that the message can be created" do
+    before(:all) do
 
         @message = Message.create(title: "Stars", description: "Best song by Switchfoot", tag: "Hits")
 
+    end
+
+    it "checks that the message can be created" do
         expect(@message).to be_valid
     end
 
     it "checks that the message can be updated" do
-
-        @message = Message.create(title: "Stars", description: "Best song by Switchfoot", tag: "Hits")
-
         @message.update(:title => "Let that be Enough")
-
         expect(Message.find_by_title("Let that be Enough")).to eq(@message)
-
     end
 
     it "checks that the message can be destroyed" do
-
-        @message = Message.create(title: "Stars", description: "Best song by Switchfoot", tag: "Hits")
-
         @message.destroy
-
         expect(Message.find_by(title: 'Let that be Enough')).to be_nil
-
     end
 
 
